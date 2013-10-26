9a37d99b335bc130ce213fb059393056bf09f431
Jordan Morano <jordan.morano@gmail.com>
2013-10-25 23:17:13 -0400

    Add posts and users; Link to DB

---
+++ views/v_users_loggedin.php
@@ -0,0 +1,1 @@
+<h2>Welcome to Morano Space, <?$user=>'first_name'?></h2>
---
+++ views/v_users_login.php
@@ -0,0 +1,10 @@
+<h2>Log In</h2>
+
+<form method='post' action='/users/p_login'>
+
+	Email <input type='email' name='email'>
+	Password <input type='password' name='password'>
+
+	<input type='submit' value='Log In'>
+
+</form>
