8b57017238011c5ed038d12b5ca19c55117a20a9
Jordan Morano <jordan.morano@gmail.com>
2013-10-29 15:38:07 -0400

    update db name

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "p2_jordanmorano_biz"); 
+	define('DB_NAME', "jordanm1_p2_jordanmorano_biz"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
